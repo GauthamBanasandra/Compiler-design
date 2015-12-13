0c0aed3a7aa0efd625c5017a608c8fb5a4291d6a
Gautham_B_A <gautham.bangalore@gmail.com>
2015-12-13 08:39:47 +0530

    Counts the comments and removes them.

---
+++ lex_programs/4/input.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	//Declare		variables.
+	int i=0;
+	/*	
+		loop
+	*/
+	int x=0;
+	/* This is a /*nested comment*/
+	for(i=0; i<10; ++i)
+		printf("loop\n");
+	//One more comment.
+	/*
+		One more 			multi-line comment.*/
+	return 0;
+}
